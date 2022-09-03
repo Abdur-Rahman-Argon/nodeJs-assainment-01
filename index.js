e748f25a00a1520d99c2896413f44d0f3cf4983d
Abdur-Rahman-Argon <106532958+Abdur-Rahman-Argon@users.noreply.github.com>
2022-09-03 10:25:06 +0600

    random user get

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+const users = require("./users.json");
+
 app.use(cors());
 app.use(express.json());
 
@@ -11,9 +13,16 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-// app.all("*", (req, res) => {
-//   res.send("NO route found.");
-// });
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+
+app.get("/users/random", (req, res) => {
+  const user = users;
+  const randomUser = user[Math.floor(Math.random() * user.length)];
+
+  res.send(randomUser);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
